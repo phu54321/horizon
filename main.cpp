05158a4c0a4cf67c8db3f6b4a4aaa4389f32d423
박현우 <phu54321@naver.com>
2024-02-09 23:29:19 +0900

    feat: tray icon

--- main.cpp
+++ main.cpp
@@ -1,14 +1,45 @@
 #include <windows.h>
-#include <stdio.h>
+#include <tchar.h>
+#include "resource.h"
+
+#define TRAY_ICON_ID 12567
+#define TRAY_NOTIFY (WM_APP + 100)
+#define MENU_QUIT_MESSAGE 0x101
 
 bool clipped = false;
 HHOOK hMouseHook;
 HHOOK hKeyboardhook;
 
+void addTrayIcon(HINSTANCE hInstance, HWND hWnd) {
+    NOTIFYICONDATA nid = {0};
+    nid.cbSize = sizeof(nid);
+    nid.hWnd = hWnd;
+    nid.uID = TRAY_ICON_ID;
+    nid.uFlags = NIF_MESSAGE | NIF_ICON | NIF_TIP;
+    nid.uCallbackMessage = TRAY_NOTIFY;
+    nid.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON));
+    _tcsncpy(nid.szTip, TEXT("Horizon"), 128);
+    Shell_NotifyIcon(NIM_ADD, &nid);
+}
+
+void deleteTrayIcon(HWND hWnd) {
+    NOTIFYICONDATA nid = {0};
+    nid.cbSize = sizeof(nid);
+    nid.hWnd = hWnd;
+    nid.uID = TRAY_ICON_ID;
+    Shell_NotifyIcon(NIM_DELETE, &nid);
+}
+
+bool lButtonClicked = false;
+
 LRESULT CALLBACK MouseEvent(int nCode, WPARAM wParam, LPARAM lParam) {
     if (nCode >= 0) {
-        if (clipped) {
-            if (wParam == WM_MOUSEMOVE) {
+        if (wParam == WM_LBUTTONDOWN) {
+            lButtonClicked = true;
+        } else if (wParam == WM_LBUTTONUP) {
+            lButtonClicked = false;
+        } else if (wParam == WM_MOUSEMOVE) {
+            if (lButtonClicked && clipped) {
                 auto params = reinterpret_cast<LPMSLLHOOKSTRUCT>(lParam);
                 POINT currentPos;
                 GetCursorPos(&currentPos);
@@ -34,7 +65,7 @@ LRESULT CALLBACK MouseEvent(int nCode, WPARAM wParam, LPARAM lParam) {
 }
 
 
-int hotkeyVKCode[] = { VK_CAPITAL };
+int hotkeyVKCode[] = {VK_CAPITAL};
 constexpr int hotkeySize = sizeof(hotkeyVKCode) / sizeof(int);
 bool hotkeyPressed[hotkeySize] = {};
 
@@ -44,10 +75,10 @@ LRESULT CALLBACK KeyEvent(int nCode, WPARAM wParam, LPARAM lParam) {
         auto params = reinterpret_cast<LPKBDLLHOOKSTRUCT>(lParam);
         if (wParam == WM_KEYDOWN) {
             bool processed = false;
-            for (int i = 0 ; i < hotkeySize ; i++) {
+            for (int i = 0; i < hotkeySize; i++) {
                 if (params->vkCode == hotkeyVKCode[i]) {
                     hotkeyPressed[i] = true;
-                    processed  = true;
+                    processed = true;
                     break;
                 }
             }
@@ -64,7 +95,7 @@ LRESULT CALLBACK KeyEvent(int nCode, WPARAM wParam, LPARAM lParam) {
             }
         } else if (wParam == WM_KEYUP) {
             bool processed = false;
-            for (int i = 0 ; i < hotkeySize ; i++) {
+            for (int i = 0; i < hotkeySize; i++) {
                 if (params->vkCode == hotkeyVKCode[i]) {
                     hotkeyPressed[i] = false;
                     processed = true;
@@ -87,11 +118,81 @@ LRESULT CALLBACK KeyEvent(int nCode, WPARAM wParam, LPARAM lParam) {
     return CallNextHookEx(hMouseHook, nCode, wParam, lParam);
 }
 
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (msg == WM_CREATE) {
+        auto hInstance = (HINSTANCE) GetWindowLongPtr(hWnd, GWLP_HINSTANCE);
+        addTrayIcon(hInstance, hWnd);
+
+        hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, (HOOKPROC) MouseEvent, hInstance, 0);
+        hKeyboardhook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC) KeyEvent, hInstance, 0);
+
+        return 0;
+    } else if (msg == WM_DESTROY) {
+        UnhookWindowsHookEx(hMouseHook);
+        UnhookWindowsHookEx(hKeyboardhook);
+
+        deleteTrayIcon(hWnd);
+        PostQuitMessage(0);
+        return 0;
+    } else if (msg == TRAY_NOTIFY) {
+        if (wParam == TRAY_ICON_ID && lParam == WM_RBUTTONUP) {
+            auto hMenu = CreateMenu();
+            AppendMenu(hMenu, MF_STRING, MENU_QUIT_MESSAGE, TEXT("Quit"));
+
+            auto hMenubar = CreateMenu();
+            AppendMenu(hMenubar, MF_POPUP, (UINT_PTR) hMenu, TEXT("Menu"));
+
+            auto hPopupMenu = GetSubMenu(hMenubar, 0);
+            POINT pt;
+            GetCursorPos(&pt);
+            SetForegroundWindow(hWnd);
+            TrackPopupMenu(hPopupMenu, TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_RIGHTBUTTON, pt.x, pt.y, 0, hWnd, NULL);
+            SetForegroundWindow(hWnd);
+            PostMessage(hWnd, WM_NULL, 0, 0);
+
+            DestroyMenu(hMenu);
+            DestroyMenu(hMenubar);
+
+            return 0;
+        }
+    } else if (msg == WM_COMMAND) {
+        auto wmId = LOWORD(wParam);
+        if (wmId == MENU_QUIT_MESSAGE) {
+            DestroyWindow(hWnd);
+            return 0;
+        }
+    }
+
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
     SetProcessDPIAware();
 
-    hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, (HOOKPROC) MouseEvent, hInstance, 0);
-    hKeyboardhook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC) KeyEvent, hInstance, 0);
+    WNDCLASS wndClass;
+    memset(&wndClass, 0, sizeof(WNDCLASS));
+    wndClass.hInstance = hInstance;
+    wndClass.lpszClassName = TEXT("delaySleepOnLidClose");
+    wndClass.lpfnWndProc = WndProc;
+    wndClass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON));
+    RegisterClass(&wndClass);
+
+    HWND hWnd = CreateWindow(
+            TEXT("delaySleepOnLidClose"),
+            TEXT("delaySleepOnLidClose"),
+            0,
+            0, 0,
+            CW_USEDEFAULT, CW_USEDEFAULT,
+            HWND_MESSAGE,
+            nullptr,
+            hInstance,
+            nullptr);
+    if (!hWnd) {
+        MessageBox(nullptr, TEXT("Cannot create window"), TEXT("Error"), MB_OK);
+        return 1;
+    }
 
     MSG msg;
     while (GetMessage(&msg, nullptr, 0, 0) > 0) {
@@ -99,8 +200,5 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         DispatchMessage(&msg);
     }
 
-    UnhookWindowsHookEx(hMouseHook);
-    UnhookWindowsHookEx(hKeyboardhook);
-
-    return msg.wParam;
+    return 0;
 }
